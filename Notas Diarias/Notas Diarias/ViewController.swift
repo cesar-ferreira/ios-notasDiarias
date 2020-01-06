eaaba6c39416c5ad646bdc7077d0c096b31595b2
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2020-01-06 11:22:52 -0300

    create new project

---
+++ Notas Diarias/Notas Diarias/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notas Diarias
+//
+//  Created by César  Ferreira on 06/01/20.
+//  Copyright © 2020 César  Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
